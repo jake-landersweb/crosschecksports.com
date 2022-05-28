ee70a912900ec4e244cc0f1bc88cab47fb0ce492
Jake Landers <jake@landersweb.com>
2022-05-27 22:49:45 -0700

    added redirect fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,25 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/header/header'
 import Footer from '../components/footer/footer'
+import HttpsRedirect from 'react-https-redirect';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-    <div className="text-txt bg-bg scroll-smooth">
-      <div className="grid place-items-center">
-        <div className="fixed top-0 z-50">
-          <Header />
-        </div>
-        <div className="relative w-full max-w-[1500px] px-2 lg:px-20 md:px-10 min-h-screen pt-[100px] pb-24 md:pb-36">
-          <Component {...pageProps} />
-        </div>
-        <div className="pt-4 w-full">
-          <Footer />
+    <HttpsRedirect>
+      <div className="text-txt bg-bg scroll-smooth">
+        <div className="grid place-items-center">
+          <div className="fixed top-0 z-50">
+            <Header />
+          </div>
+          <div className="relative w-full max-w-[1500px] px-2 lg:px-20 md:px-10 min-h-screen pt-[100px] pb-24 md:pb-36">
+            <Component {...pageProps} />
+          </div>
+          <div className="pt-4 w-full">
+            <Footer />
+          </div>
         </div>
       </div>
-    </div>
+    </HttpsRedirect>
   </>
 }
 

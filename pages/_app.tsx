147a569d49473a5bde1b68ddf348b6726be26a2d
Jake Landers <jake@landersweb.com>
2022-05-29 20:33:56 -0700

    added anylatics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,22 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="author" content="Landersweb LLC" />
         <meta name="keywords" content="sports, team, management, customization" id="keywords" />
       </Head>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-JJ9RT96D8Q`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JJ9RT96D8Q', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }} />
       <div className="text-txt bg-bg scroll-smooth">
         <div className="grid place-items-center">
           <div className="fixed top-0 z-50">
